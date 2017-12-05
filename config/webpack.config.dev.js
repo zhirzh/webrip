9d45244c6c284b38af200f505b2493dc38afde58
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2017-12-05 16:14:33 +0530

    add npm scripts to build chrome plugin

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -35,17 +35,17 @@ module.exports = {
   entry: [
     // We ship a few polyfills by default:
     require.resolve('./polyfills'),
-    // Include an alternative client for WebpackDevServer. A client's job is to
-    // connect to WebpackDevServer by a socket and get notified about changes.
-    // When you save a file, the client will either apply hot updates (in case
-    // of CSS changes), or refresh the page (in case of JS changes). When you
-    // make a syntax error, this client will display a syntax error overlay.
-    // Note: instead of the default WebpackDevServer client, we use a custom one
-    // to bring better experience for Create React App users. You can replace
-    // the line below with these two lines if you prefer the stock client:
-    // require.resolve('webpack-dev-server/client') + '?/',
-    // require.resolve('webpack/hot/dev-server'),
-    require.resolve('react-dev-utils/webpackHotDevClient'),
+    // // Include an alternative client for WebpackDevServer. A client's job is to
+    // // connect to WebpackDevServer by a socket and get notified about changes.
+    // // When you save a file, the client will either apply hot updates (in case
+    // // of CSS changes), or refresh the page (in case of JS changes). When you
+    // // make a syntax error, this client will display a syntax error overlay.
+    // // Note: instead of the default WebpackDevServer client, we use a custom one
+    // // to bring better experience for Create React App users. You can replace
+    // // the line below with these two lines if you prefer the stock client:
+    // // require.resolve('webpack-dev-server/client') + '?/',
+    // // require.resolve('webpack/hot/dev-server'),
+    // require.resolve('react-dev-utils/webpackHotDevClient'),
     // Finally, this is your app's code:
     paths.appIndexJs,
     // We include the app code last so that if there is a runtime error during
@@ -53,6 +53,7 @@ module.exports = {
     // changing JS code would still trigger a refresh.
   ],
   output: {
+    path: paths.appBuild,
     // Add /* filename */ comments to generated require()s in the output.
     pathinfo: true,
     // This does not produce a real file. It's just the virtual path that is
@@ -226,8 +227,8 @@ module.exports = {
     // Makes some environment variables available to the JS code, for example:
     // if (process.env.NODE_ENV === 'development') { ... }. See `./env.js`.
     new webpack.DefinePlugin(env.stringified),
-    // This is necessary to emit hot updates (currently CSS only):
-    new webpack.HotModuleReplacementPlugin(),
+    // // This is necessary to emit hot updates (currently CSS only):
+    // new webpack.HotModuleReplacementPlugin(),
     // Watcher doesn't work well if you mistype casing in a path so we use
     // a plugin that prints an error when you attempt to do this.
     // See https://github.com/facebookincubator/create-react-app/issues/240
---
+++ copy-public.js
@@ -0,0 +1,14 @@
+const { resolve } = require('path');
+const { execSync } = require('child_process');
+const { existsSync, mkdirSync } = require('fs');
+
+const PUBLIC_DIR = resolve(__dirname, 'public');
+const BUILD_DIR = resolve(__dirname, 'build');
+
+if (!existsSync(BUILD_DIR)) {
+  mkdirSync(BUILD_DIR);
+} else {
+  execSync(`rm -r ${BUILD_DIR}/*`);
+}
+
+execSync(`cp -r ${PUBLIC_DIR}/* ${BUILD_DIR}`);
